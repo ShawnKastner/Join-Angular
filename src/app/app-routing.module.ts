af6ec6c535c70ff623818d4a70dfb71088e0cc76
ShawnKastner <shawn.kastner@icloud.com>
2023-06-07 15:23:50 +0200

    set it up the forgot password page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 const routes: Routes = [
   { path: '', component: SignInComponent },
   { path: 'register', component: SignUpComponent },
+  { path: 'forgotPassword', component: ForgotPasswordComponent },
 ];
 
 @NgModule({
