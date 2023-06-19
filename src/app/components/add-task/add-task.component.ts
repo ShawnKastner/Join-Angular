9a7f9054c6614e4c8104acab9a7c07207d167656
ShawnKastner <shawn.kastner@icloud.com>
2023-06-19 11:08:24 +0200

    add subtasks and render this with checkboxes and add a delete function

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -5,11 +5,13 @@ import {
   addDoc,
   collection,
   collectionData,
+  getDocs,
 } from '@angular/fire/firestore';
 import { MatDialog } from '@angular/material/dialog';
 import { Task } from 'src/app/models/tasks.model';
 import { NewCategoryDialogComponent } from './new-category-dialog/new-category-dialog.component';
 import { Observable } from 'rxjs';
+import { deleteDoc } from 'firebase/firestore';
 
 @Component({
   selector: 'app-add-task',
@@ -26,20 +28,43 @@ export class AddTaskComponent implements OnInit {
   date!: string;
   selectedPriority!: string;
   subtask!: string;
+  selectedSubtasks: { [key: string]: boolean } = {};
 
   categories$!: Observable<any[]>;
   categories: any[] = [];
+
+  subtasks$!: Observable<any[]>;
+  subtasks: any[] = [];
 
   constructor(private firestore: Firestore, private dialog: MatDialog) {
     this.minDate = new Date();
   }
 
   ngOnInit(): void {
+    this.getCategoriesFromFirestore();
+    this.getSubtasksFromFirestore();
+  }
+
+  getSubtasksFromFirestore() {
+    this.subtasks$ = collectionData(collection(this.firestore, 'subtasks'));
+    this.subtasks$.subscribe((data) => {
+      this.subtasks = data;
+      console.log(this.subtasks);
+    });
+  }
+
+  getCategoriesFromFirestore() {
     this.categories$ = collectionData(collection(this.firestore, 'categories'));
     this.categories$.subscribe((data) => {
       this.categories = data;
       console.log(this.categories);
     });
+  }
+
+  getSelectedSubtasks(): string[] {
+    return Object.keys(this.selectedSubtasks).filter(
+      (key) => this.selectedSubtasks[key]
+    );
   }
 
   addTask() {
@@ -51,7 +76,7 @@ export class AddTaskComponent implements OnInit {
       this.selectedContact,
       new Date(this.date),
       this.selectedPriority,
-      this.subtask
+      this.getSelectedSubtasks().join(',')
     );
     addDoc(collectionRef, { ...newTask })
       .then(() => {
@@ -71,6 +96,7 @@ export class AddTaskComponent implements OnInit {
     this.date = '';
     this.selectedPriority = '';
     this.subtask = '';
+    this.selectedSubtasks = {};
   }
 
   openAddCategoryDialog() {
@@ -80,13 +106,36 @@ export class AddTaskComponent implements OnInit {
   }
 
   getCategoryColor(category: string): string {
-    const selectedCategory = this.categories.find((item) => item.name === category);
+    const selectedCategory = this.categories.find(
+      (item) => item.name === category
+    );
     return selectedCategory ? selectedCategory.color : '';
   }
-  
+
   getCategoryName(category: string): string {
-    const selectedCategory = this.categories.find((item) => item.name === category);
+    const selectedCategory = this.categories.find(
+      (item) => item.name === category
+    );
     return selectedCategory ? selectedCategory.name : '';
   }
-  
+
+  addSubtask() {
+    const subTaskCollection = collection(this.firestore, 'subtasks');
+    addDoc(subTaskCollection, {
+      subtask: this.subtask,
+    }).then(() => {
+      this.subtask = '';
+    });
+  }
+
+  async deleteAllSubtasks() {
+    const subTaskCollectionRef = collection(this.firestore, 'subtasks');
+    const querySnapshot = await getDocs(subTaskCollectionRef);
+    
+    querySnapshot.forEach(async (doc) => {
+      await deleteDoc(doc.ref);
+      console.log('Subtask deleted successfully');
+    });
+    
+  }
 }
