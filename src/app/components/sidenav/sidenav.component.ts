8e48b2b67069473aeabb6db9dd91635780dbc26a
ShawnKastner <shawn.kastner@icloud.com>
2023-07-06 11:48:25 +0200

    change position of logout dialog at mobile devices

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,24 +1,29 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { LogoutDialogComponent } from './logout-dialog/logout-dialog.component';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent {
-
-  constructor(
-    public authService: AuthService,
-    public dialog: MatDialog
-  ) {}
+  constructor(public authService: AuthService, public dialog: MatDialog) {}
 
   openLogoutDialog(): void {
-		this.dialog.open(LogoutDialogComponent, {
-			width: '130px',
-			position: { top: '89px', right: '0px' },
-		});
-	}
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.panelClass = ['position-dialog'];
+
+    if (window.innerWidth <= 1000) {
+      dialogConfig.position = { top: '79px', right: '0px' };
+    } else {
+      dialogConfig.position = { top: '89px', right: '0px' };
+    }
+    this.dialog.open(LogoutDialogComponent, {
+      width: '130px',
+      ...dialogConfig,
+      position: dialogConfig.position,
+    });
+  }
 }
