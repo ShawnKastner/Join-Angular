6af97fa86fabfdc57f9b8f436d5391c717ed0a6b
ShawnKastner <shawn.kastner@icloud.com>
2023-07-10 13:52:41 +0200

    added remember me function and create a function to encode the password at the localstorage

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
@@ -6,9 +10,67 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss'],
 })
-export class SignInComponent {
-  hide = true;
-  rememberMe = false;
+export class SignInComponent implements OnInit {
+  @ViewChild('userEmail', { static: true })
+  userEmailElement!: ElementRef<HTMLInputElement>;
+  @ViewChild('userPassword', { static: true })
+  userPasswordElement!: ElementRef<HTMLInputElement>;
 
-  constructor(public authService: AuthService) {}
+  hide = true;
+  rememberMe!: boolean;
+
+  constructor(
+    public authService: AuthService,
+    private afAuth: AngularFireAuth,
+    private firestore: AngularFirestore
+  ) {
+    afAuth.setPersistence(firebase.auth.Auth.Persistence.LOCAL);
+  }
+
+  ngOnInit() {
+    this.checkRememberedCredentials();
+  }
+
+  toggleRememberMe() {
+    if (this.rememberMe) {
+      const userEmail = this.userEmailElement.nativeElement.value;
+      const userPassword = this.userPasswordElement.nativeElement.value;
+
+      if (userEmail) {
+        // Überprüfung, ob die E-Mail-Adresse angegeben wurde
+        const encodedPassword = btoa(userPassword);
+
+        localStorage.setItem('rememberedEmail', userEmail);
+        localStorage.setItem('rememberedPassword', encodedPassword);
+
+        this.afAuth.setPersistence(firebase.auth.Auth.Persistence.LOCAL);
+      }
+    } else {
+      localStorage.removeItem('rememberedEmail');
+      localStorage.removeItem('rememberedPassword');
+
+      this.afAuth
+        .setPersistence(firebase.auth.Auth.Persistence.NONE)
+        .then(() => {
+          return this.afAuth.signOut();
+        })
+        .catch((error) => {
+          console.log('Error removing remembered credentials:', error);
+        });
+    }
+  }
+
+  checkRememberedCredentials() {
+    const rememberedEmail = localStorage.getItem('rememberedEmail');
+    const encodedPassword = localStorage.getItem('rememberedPassword');
+
+    if (rememberedEmail && encodedPassword) {
+      this.rememberMe = true;
+      this.userEmailElement.nativeElement.value = rememberedEmail;
+
+      const decodedPassword = atob(encodedPassword);
+
+      this.userPasswordElement.nativeElement.value = decodedPassword;
+    }
+  }
 }
